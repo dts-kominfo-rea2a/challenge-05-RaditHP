d2aded0b4627688fe22f7174f373a8d901d7bbf9
Rakha HP <rpramudito@gmail.com>
2022-06-04 16:23:23 +0700

    Ini adalah solusi dari challenge 5

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = function (array, fnCallback) {
+  sortedNumbered = [];
+  sortedNames = [];
+  sortedNames = fnCallback(array);
+  for (counter = 0; counter < sortedNames.length; counter++) {
+    sortedNumbered.push(`${counter + 1}. ${array[counter]}`);
+  }
+  return sortedNumbered;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = function (names) {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = function (names) {
+  return names.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
